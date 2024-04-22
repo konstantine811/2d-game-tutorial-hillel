dcf01f6f8eadd2c9474d379fff693122cff72287
k.abramkin <k.abramkin@kernel.ua>
2024-04-22 20:11:46 +0300

    add first canvas graphic element

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 567;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
